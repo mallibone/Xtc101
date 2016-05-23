cb6307a6204fb453e3033fd75fdc277544d346b2
Mark Allibone <mallibone@outlook.com>
2016-05-23 20:17:31 +0200

    Adds UI test for android app

---
+++ Xtc101.UITest/AppInitializer.cs
@@ -0,0 +1,23 @@
+﻿using Xamarin.UITest;
+
+namespace Xtc101.UITest
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp
+                    .Android
+                    .Debug()
+                    .StartApp(Xamarin.UITest.Configuration.AppDataMode.Clear);
+            }
+
+            return ConfigureApp
+                .iOS
+                .PreferIdeSettings()
+                .StartApp();
+        }
+    }
+}
---
+++ Xtc101.UITest/Tests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+using Xamarin.UITest.Android;
+
+namespace Xtc101.UITest
+{
+    [TestFixture(Platform.Android)]
+    [TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        private Platform _platform;
+
+        public Tests(Platform platform)
+        {
+            _platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            // TODO: If the Android app being tested is included in the solution then open
+            // the Unit Tests window, right click Test Apps, select Add App Project
+            // and select the app projects that should be tested.
+            app = AppInitializer
+                // TODO: Update this path to point to your Android app and uncomment the
+                // code if the app is not included in the solution.
+                //.ApkFile ("../../../Android/bin/Debug/UITestsAndroid.apk")
+                .StartApp(_platform);
+        }
+
+        [Test]
+        public void SubmittingAMessage_GivenATextIsEnteredAndSubmitted_ItCanBeReadFromTheSubmittedMessageField()
+        {
+            var expectedResult = "Hello Android";
+
+            app.EnterText(c => c.Marked("MessageText"), expectedResult);
+            app.Tap(c => c.Marked("SubmitMessage"));
+
+            var receivedResult = app.Query(c => c.Marked("SubmittedMessage")).First().Text;
+            Assert.AreEqual(expectedResult, receivedResult);
+        }
+    }
+}
+
